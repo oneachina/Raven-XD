8dd1964700c3726372f28f3ffca2838853fd2173
oneachina <ruiruixi@hotmail.com>
2024-11-16 16:45:02 +0800

    妈的不想写了

---
+++ src/main/java/cn/onea/keystrokesmod/module/setting/impl/InputSetting.java
@@ -0,0 +1,11 @@
+package cn.onea.keystrokesmod.module.setting.impl;
+
+public interface InputSetting {
+    double getInput();
+
+    void setValue(double value);
+
+    double getMin();
+
+    double getMax();
+}
